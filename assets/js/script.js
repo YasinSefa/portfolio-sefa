a2d86325bb6d92a67c4bb220ef250e045d2a5e07
YasinSefa <ysnsfaksoy@gmail.com>
2024-03-18 16:17:41 +0300

    18.03/6

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Sefa";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/logo.png");
         }
         else {
             document.title = "Come Back To Portfolio";
