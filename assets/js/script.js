54add5ecde5a29545ef721c7a1641c1846d23668
YasinSefa <ysnsfaksoy@gmail.com>
2024-03-18 16:05:33 +0300

    18.03/5

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web development"],
+    strings: ["frontend developer", "backend developer", "web developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
