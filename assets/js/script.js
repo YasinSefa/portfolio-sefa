6bb4a8fcb8898455779a802221148b02127f1022
YasinSefa <ysnsfaksoy@gmail.com>
2024-03-25 11:31:26 +0300

    25.03/1

--- assets/js/script.js
+++ assets/js/script.js
@@ -213,3 +213,7 @@ srtop.reveal('.work .box', { interval: 200 });
 /* SCROLL EXPERIENCE */
 srtop.reveal('.experience .timeline', { delay: 400 });
 srtop.reveal('.experience .timeline .container', { interval: 400 });
+
+// Send Email Contact
+
+
